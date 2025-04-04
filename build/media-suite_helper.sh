4c8dba3ac7fb3df3fb11abc242d4c8db49187ca3
Greg Scaffidi <sgscaffidi3@gmail.com>
2025-04-04 11:15:03 -0500

    Update media-suite_helper.sh

--- build/media-suite_helper.sh
+++ build/media-suite_helper.sh
@@ -1276,8 +1276,10 @@ do_patch() {
     if [[ -f $patchName ]]; then
         if $am; then
             git update-index --refresh > /dev/null 2>&1 &&
-            git apply -3 --ignore-space-change --ignore-whitespace "$patchName" > /dev/null 2>&1 &&
+            git apply -3 --check --ignore-space-change --ignore-whitespace "$patchName" > /dev/null 2>&1 &&
+                git am -q -3 --ignore-whitespace --no-gpg-sign "$patchName" > /dev/null 2>&1 &&
                 return 0
+            git am -q --abort > /dev/null 2>&1
         else
             patch --dry-run $binarypatch -s -N -p"$strip" -i "$patchName" > /dev/null 2>&1 &&
                 patch $binarypatch -s -N -p"$strip" -i "$patchName" &&
